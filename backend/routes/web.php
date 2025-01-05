2398773c17a4e8cbc8942128a54d7282e5bb5540
Oussama Darrazi <nostra.darrazi2002@gmail.com>
2025-01-05 13:52:44 +0100

    Initial commit

---
+++ backend/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/test-mongo', function () {
+    try {
+        $databases = DB::connection('mongodb')->getMongoClient()->listDatabases();
+        return response()->json($databases);
+    } catch (\Exception $e) {
+        return response()->json(['error' => $e->getMessage()], 500);
+    }
+});
